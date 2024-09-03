8e6f04977dfae24ea1731fed8596138c76b774e6
islamzhumabaev <129617690+islamzhumabaev@users.noreply.github.com>
2024-09-03 19:48:14 +0500

    Add files via upload

---
+++ testBoot/src/main/java/kz/zhumabayev/testBoot/SpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package kz.zhumabayev.testBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+}
